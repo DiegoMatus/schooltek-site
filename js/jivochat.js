7f920b3c6240a3cf261a0b81597ba918b2754ef2
Diego Matus Perdomo <d.m.perdomo@evomatik.com>
2017-04-03 11:48:16 -0500

    feat - cambios en chat de jivo por zemdesk

--- js/jivochat.js
+++ js/jivochat.js
@@ -1,23 +1,5 @@
-(function(){
-        var widget_id = '5vh8YdyCSc';
-        var d=document;
-        var w=window;
-
-        function l(){
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = '//code.jivosite.com/script/widget/'+widget_id;
-            var ss = document.getElementsByTagName('script')[0];
-            ss.parentNode.insertBefore(s, ss);
-        }
-        if(d.readyState=='complete'){
-            l();
-        }else{
-            if(w.attachEvent){
-                w.attachEvent('onload',l);
-            }else{
-                w.addEventListener('load',l,false);
-            }
-        }
-    })();
+window.$zopim||(function(d,s){var z=$zopim=function(c){z._.push(c)},$=z.s=
+d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute("charset","utf-8");
+$.src="https://v2.zopim.com/?4g4cAA5l5XN2gYFEdsbRjY5TfqcBpIPd";z.t=+new Date;$.
+type="text/javascript";e.parentNode.insertBefore($,e)})(document,"script");
